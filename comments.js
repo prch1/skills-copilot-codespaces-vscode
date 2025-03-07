5231afcaa8325c61bce1419eeee1944b9a4767df
Paulo Ricardo <pauloricardoh@gmail.com>
2025-03-07 16:04:04 +0000

    Copilot third commit 2

--- comments.js
+++ comments.js
@@ -1,14 +1,42 @@
-const http = require('http');
+//Create web server             
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+var comments = [];
+var server = http.createServer(function(req,res){
+    var parseUrl = url.parse(req.url,true);
+    var pathName = parseUrl.pathname;
+    if(pathName ===
+        '/'){
+        fs.readFile('./index.html',function(err,data){
+            if(err){
+                console.log(err);
+                res.end('404 Not Found');
+            }
+            res.end(data);
+        });
+    }       
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
-});
+    else if(pathName === '/comment'){
+        var comment = parseUrl.query.comment;
+        comments.push(comment);
+        res.end(JSON.stringify(comments));
+    }
+    else{
+        fs.readFile(path.join
+            (__dirname,pathName),function(err,data){
+            if(err){
+                console.log(err);
+                res.end('404 Not Found');
+            }
+            res.end(data);
+        });
+    }
+}
+);
+server.listen(3000,function(){
+    console.log('Server is running');
+});     
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
